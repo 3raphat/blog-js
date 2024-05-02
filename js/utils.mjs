cbb4cc03a20c0ae22fdb9820ff5c080d91f20b32
3raphat <treeraphat234@gmail.com>
2024-05-03 04:12:59 +0700

    display author and date

--- js/utils.mjs
+++ js/utils.mjs
@@ -8,4 +8,41 @@ function debounce(func, delay) {
   }
 }
 
-export { debounce }
+function formatDistanceToNow(date) {
+  const targetDate = new Date(date)
+  const now = new Date()
+  const distance = now - targetDate
+
+  if (distance < 1000) {
+    return "Just now"
+  }
+
+  const seconds = Math.floor(distance / 1000)
+  if (seconds < 60) {
+    return `${seconds} seconds ago`
+  }
+
+  const minutes = Math.floor(seconds / 60)
+  if (minutes < 60) {
+    return `${minutes} minutes ago`
+  }
+
+  const hours = Math.floor(minutes / 60)
+
+  if (hours < 24) {
+    return `${hours} hours ago`
+  }
+
+  const days = Math.floor(hours / 24)
+  if (days <= 1) {
+    return `${days} day ago`
+  }
+
+  return targetDate.toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  })
+}
+
+export { debounce, formatDistanceToNow }
