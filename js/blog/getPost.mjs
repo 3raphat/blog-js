97311c132ea30dea450c4821df071120cb5d6675
3raphat <treeraphat234@gmail.com>
2024-05-09 03:08:54 +0700

    feat: add infinite scroll

--- js/blog/getPost.mjs
+++ js/blog/getPost.mjs
@@ -1,11 +1,16 @@
-export async function getAllPostsByName(name, tag) {
+export async function getAllPostsByName(name, tag, limit, page) {
   try {
+    const params = new URLSearchParams()
+    if (tag) params.append("_tag", tag)
+    if (limit) params.append("limit", limit)
+    if (page) params.append("page", page)
+
     const response = await fetch(
-      `https://v2.api.noroff.dev/blog/posts/${name}${tag ? `?_tag=${tag}` : ""}`
+      `https://v2.api.noroff.dev/blog/posts/${name}?${params.toString()}`
     )
     const posts = await response.json()
 
-    return posts.data
+    return posts
   } catch (error) {
     console.error(error)
   }
